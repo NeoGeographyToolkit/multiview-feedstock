f496a89da92052ca99670dab6dfbe0d21f0554b7
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-12-27 20:34:34 -0800

    Update for ASP 3.2.0 release

---
+++ recipe/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Recommended in https://gitter.im/conda-forge/conda-forge.github.io?at=5c40da7f95e17b45256960ce
+find ${PREFIX}/lib -name '*.la' -delete
+
+git submodule update --init --recursive
+
+mkdir build && cd build
+
+cmake                                       \
+    -DCMAKE_BUILD_TYPE=Release              \
+    -DMULTIVIEW_DEPS_DIR=${PREFIX}          \
+    -DCMAKE_VERBOSE_MAKEFILE=ON             \
+    -DCMAKE_CXX_FLAGS='-O3 -std=c++11'      \
+    -DCMAKE_C_FLAGS='-O3'                   \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}
+
+make -j${CPU_COUNT}
+make install
+
